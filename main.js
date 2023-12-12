7cc571e6681738dfbd09664971e0f276e9c5ed49
Potter <75838259@qq.com>
2023-12-12 11:48:07 +0800

    building first app

---
+++ main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require("electron");
+
+const createWindow = () => {
+	const win = new BrowserWindow({
+		width: 800,
+		height: 600,
+	});
+
+	win.loadFile("index.html");
+};
+
+app.whenReady().then(() => {
+	createWindow();
+	console.log(process.platform);
+
+	app.on("activate", () => {
+		if (BrowserWindow.getAllWindows().length === 0) {
+			createWindow();
+		}
+	});
+});
+
+app.on("window-all-closed", () => {
+	if (process.platform !== "darwin") {
+		console.log("quit");
+		app.quit();
+	}
+});
