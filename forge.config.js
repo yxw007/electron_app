59be5423a108a3476aa8a66992ed126690f3606b
Potter <75838259@qq.com>
2023-12-13 14:46:13 +0800

    add: 添加打包证书

--- forge.config.js
+++ forge.config.js
@@ -1,30 +1,33 @@
 module.exports = {
-  packagerConfig: {
-    asar: true,
-  },
-  rebuildConfig: {},
-  makers: [
-    {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
-  ],
-  plugins: [
-    {
-      name: '@electron-forge/plugin-auto-unpack-natives',
-      config: {},
-    },
-  ],
+	packagerConfig: {
+		asar: true,
+	},
+	rebuildConfig: {},
+	makers: [
+		{
+			name: "@electron-forge/maker-squirrel",
+			config: {
+				certificateFile: "./cert/server.pfx",
+				certificatePassword: "7758258",
+			},
+		},
+		{
+			name: "@electron-forge/maker-zip",
+			platforms: ["darwin"],
+		},
+		{
+			name: "@electron-forge/maker-deb",
+			config: {},
+		},
+		{
+			name: "@electron-forge/maker-rpm",
+			config: {},
+		},
+	],
+	plugins: [
+		{
+			name: "@electron-forge/plugin-auto-unpack-natives",
+			config: {},
+		},
+	],
 };
